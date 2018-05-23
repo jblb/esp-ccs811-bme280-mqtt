df4a1aac0e1fc07c418e403683ba749345c1c399
jblb <jerome@jblb.net>
2018-05-23 10:17:40 +0200

    more clean up code

--- esp-ccs811-bme280-mqtt/config.example.h
+++ esp-ccs811-bme280-mqtt/config.example.h
@@ -2,25 +2,25 @@
 
 /*
  * Main Settings
- * 
+ *
  * measureDelay: defines how often data will be pulled from the sensors (in ms, means 60000 = 60s)
  */
 const unsigned long measureDelay = 60000;
 
 /*
  * These are the current Wifi Settings
- * 
+ *
  * ssid: wifi name
  * password: wifi access key
- * 
+ *
  * hostname and OTA name are defined by mqttClientName below
  */
 const char* ssid = "<your-wifi-ssid>";
 const char* password = "<your-wifi-key>";
 
 /*
- * Settings for the MQTT Broker, which shall be used 
- * 
+ * Settings for the MQTT Broker, which shall be used
+ *
  * mqttServer: IP or Hostname of mqttBroker
  * mqttUser: username on MQTT server
  * mqttPass: users password
@@ -35,15 +35,15 @@ const char* mqttTopicPrefix = "<mqtt-topic-prefix>";
 
 /*
  * GPIO Settings
- * 
- * LED_BUILTIN: Digial Pin Number of your boards built-in LED or 
+ *
+ * LED_BUILTIN: Digial Pin Number of your boards built-in LED or
  *               the LED you wish to show status signals
  */
 #define LED_BUILTIN 2 //ESP-12F has the builtin LED on GPIO2, comment for other boards
 
 /*
  * CCS811
- * 
+ *
  * WAKE_PIN: Digial Pin Number, where your BME280s wake signal is connected
  */
 #define WAKE_PIN 15
